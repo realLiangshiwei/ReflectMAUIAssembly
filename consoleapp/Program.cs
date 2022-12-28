7d4fbdcd1ce9390e5f3852098b8ccb8b5830a1b1
liangshiwei <deawback@hotmail.com>
2022-12-28 19:59:14 +0800

    Init

---
+++ consoleapp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+
+var assembly = Assembly.LoadFrom("...maui app's assembly file path");
+
+Console.WriteLine("Hello, World!");
+
---
+++ myapp2/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace myapp2;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
